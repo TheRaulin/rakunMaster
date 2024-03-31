a87c89624976b25d16ac155bfcaf868bd08c2999
TheRaulin <raulbarboza0114@gmail.com>
2024-03-31 16:49:08 -0500

    Organizacion de espacios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,39 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen flex flex-row bg-gray-100 dark:bg-gray-900">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap">
+
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="h-screen font-sans antialiased">
+    <div class="h-full">
+        <header class="flex flex-row justify-between shadow border-b border-gray-300" style="height: 9%">
+            @include('layouts.nav-bar')
+        </header>
+        <div class="flex flex-row bg-gray-100 " style="height: 91%">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
-            <!--@if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif-->
 
             <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
---
+++ resources/views/layouts/nav-bar.blade.php
@@ -0,0 +1,17 @@
+<div class="" style=""></div>
+<div class="flex flex-row font-lato px-6 py-2 border-l border-r border-gray-300" style="background-color: #F2F2F2; color: #444444">
+    <div class="caja_nombre px-2 text-left">
+        <div class="text-sm">Caja:</div>
+        <div class="text-lg">Bar</div>
+    </div>
+    <div class="caja_balance px-2 text-left">
+        <div class="text-sm">Balance:</div>
+        <div class="text-lg">COP 300.00000000</div>
+    </div>
+    <div class="caja_boton">
+    <a href="#modalcerrar" data-toggle="modal" id="botoncajax" class=" btn tooltip-on-hover" style="display: flex; align-items: center; border-radius: 10px; background-color: #ef4444; color: white;" title="" data-original-title="Cerrar caja">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="margin-right: 2px; color: white; width: 24px; height: 24px; padding: 2px;"><path fill="currentColor" d="M12.917 13A6.002 6.002 0 0 1 1 12a6 6 0 0 1 11.917-1H23v2h-2v4h-2v-4h-2v4h-2v-4h-2.083zM7 16a4 4 0 1 0 0-8 4 4 0 0 0 0 8z"></path></svg> <span>Cerrar la caja</span>
+                                    </a>
+    </div>
+</div>
+<div class="" style=""></div>
